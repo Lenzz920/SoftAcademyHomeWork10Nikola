721bc704bb37dfed471d66fc4be962a745757535
nikohightower@gmail.com <hWUBB-H9ux4aPmK>
2023-07-01 20:06:41 +0300

    hw

--- src/CreditBankAccount.java
+++ src/CreditBankAccount.java
@@ -1,2 +1,47 @@
 public class CreditBankAccount {
+
+    private final String nameOfCreditOwner;
+    private final double principal;
+    private final double creditTotalInterestAmount;
+    private final double yearlyInterestRate;
+    private final double years;
+    private final static String BANK_ACCOUNT_TYPE = "Credit Type";
+
+    public CreditBankAccount(String nameOfCreditOwner, double principal, double yearlyInterestRate, double years) {
+        this.nameOfCreditOwner = nameOfCreditOwner;
+        this.principal = principal;
+        this.yearlyInterestRate = yearlyInterestRate / 100;
+        this.years = years;
+        this.creditTotalInterestAmount = principal * this.yearlyInterestRate * years;
+    }
+
+    public double totalCreditAmount() {
+        return principal + creditTotalInterestAmount;
+    }
+
+    public double amountRemaining(int monthsPaid) {
+        return totalCreditAmount() - (monthsPaid * creditInstallment());
+    }
+
+    //payments are owned each month
+    public double creditInstallment() {
+        return totalCreditAmount() / (years * 12);
+    }
+
+    public double getCreditTotalInterestAmount() {
+        return creditTotalInterestAmount;
+    }
+
+    public double getYearlyInterestRate() {
+        return yearlyInterestRate;
+    }
+
+    public String getNameOfCreditOwner() {
+        return nameOfCreditOwner;
+    }
+
+    public String getBankAccountType() {
+        return BANK_ACCOUNT_TYPE;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        CreditBankAccount account = new CreditBankAccount("Bob", 5000, 10, 5);
+
+        System.out.println(account.getCreditTotalInterestAmount());
+        System.out.println(account.getYearlyInterestRate());
+        System.out.println(account.totalCreditAmount());
+        System.out.println(account.creditInstallment());
+        System.out.println(account.amountRemaining(2));
+        System.out.println(account.getBankAccountType());
+        System.out.println(account.getNameOfCreditOwner());
+
     }
 }
